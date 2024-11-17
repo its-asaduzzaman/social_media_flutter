07cd07944f691106418c63a2a7c78bd90c6dfda9
Asaduzzaman_mac <samrat.asad@gmail.com>
2024-11-17 15:11:06 +0600

    add user info to database name User document

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_flutter/components/my_button.dart';
@@ -45,18 +46,36 @@ class _RegisterPageState extends State<RegisterPage> {
       //try creating the user
       try {
         //create the user
-        UserCredential? userCredential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(
-                email: emailController.text, password: passwordController.text);
+        UserCredential? userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+
+        //create a user document and add to firestore
+        createUserDocument(userCredential);
 
         //pop the circle
-        Navigator.pop(context);
+        if (context.mounted) Navigator.pop(context);
       } on FirebaseAuthException catch (e) {
         //pop loading circle
         Navigator.pop(context);
         //display message to user
         displayMessageToUser(e.code, context);
       }
+    }
+  }
+
+  //create a user document and collect them in firestore
+  Future<void> createUserDocument(UserCredential? userCredential) async {
+    if (userCredential != null && userCredential.user != null) {
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(userCredential.user!.email)
+          .set({
+        'email': userCredential.user!.email,
+        'username': fullNameController.text,
+      });
     }
   }
 
