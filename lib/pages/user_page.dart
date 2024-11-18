c202792b4b087ba38ae9f878ec11d7d677204d94
Asaduzzaman_mac <samrat.asad@gmail.com>
2024-11-18 14:19:15 +0600

    show user info on profile page and user page from db

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_flutter/helper/helper_function.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
@@ -8,6 +10,41 @@ class UserPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("User page"),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
+        builder: (context, snapshot) {
+          //any error
+          if (snapshot.hasError) {
+            displayMessageToUser("something went wrong", context);
+          }
+
+          //loading circle
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.data == null) {
+            return const Text("No Data");
+          }
+
+          //get all users
+          final users = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              //get individual user
+              final user = users[index];
+              return ListTile(
+                title: Text(user['username']),
+                subtitle: Text(user['email']),
+
+              );
+            },
+          );
+        },
       ),
     );
   }
